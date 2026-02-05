7d399dfaec9b0705bd0ca134dcd6be84eeff5ff8
Aadit Chhetri <kingaadit3@gmail.com>
2026-02-05 15:28:18 +0545

    Add program for question 3

--- doc_3.md
+++ doc_3.md
@@ -1,15 +1,28 @@
 # Program 3 Documentation
 
 ## Data Structures
-[Describe the data structures used in this program]
+- **struct node**: Simple linked list node containing integer data and pointer to next node
+- **head**: Global pointer to the first node of the linked list
 
 ## Functions Implemented
-- **Function Name**: [Purpose of the function]
+- **insert(int val)**: Inserts a new node at the beginning of the linked list
+- **display()**: Traverses and prints the linked list in normal order
+- **reverse_print(struct node *temp)**: Recursively traverses and prints the linked list in reverse order
 
 ## Main Method Organization
-[Explain how main() is structured and the flow of execution]
+1. Reads number of nodes from user
+2. Creates linked list by inserting values at the beginning
+3. Displays the normal list
+4. Displays the reverse traversal using recursion
 
 ## Sample Output
 ```
-[Paste sample output here]
+enter no of nodes: 5
+enter value: 10
+enter value: 20
+enter value: 30
+enter value: 40
+enter value: 50
+normal list: 50 40 30 20 10 
+reverse traversal: 10 20 30 40 50
 ```
--- prog_3.c
+++ prog_3.c
@@ -1,1 +1,56 @@
 //3. Write and Implement the reverse traversal algorithm in the the linked list. Write main() to demonstrate the use of the function.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *head=NULL;
+
+void insert(int val){
+    struct node *new=(struct node*)malloc(sizeof(struct node));
+    new->data=val;
+    new->next=head;
+    head=new;
+}
+
+void display(){
+    struct node *temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+//reverse traversal using recursion
+void reverse_print(struct node *temp){
+    if(temp==NULL)
+        return;
+    reverse_print(temp->next);
+    printf("%d ",temp->data);
+}
+
+int main(){
+    int n,val,i;
+    printf("enter no of nodes: ");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        printf("enter value: ");
+        scanf("%d",&val);
+        insert(val);
+    }
+    
+    printf("normal list: ");
+    display();
+    
+    printf("reverse traversal: ");
+    reverse_print(head);
+    printf("\n");
+    
+    return 0;
+}
